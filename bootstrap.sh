8b5c090cbf41488bcc594e7921d6349547580358
Kim Thostrup <kim@thostrup.dk>
2021-06-22 15:41:46 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,14 @@ echo "wan-admin ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 echo "Disabling netplan and changing interface names"
 sudo apt install ifupdown net-tools
 sudo mv /etc/default/grub /etc/default/grub.org
-sudo wget --no-check-certificate --content-disposition https://gist.githubusercontent.com/kimthostrup/900cdfec4c45932bb3270242904b2e9d/raw/b61dd82a84759a2462113a258f2a7a4a2fc51c49/grub -P /etc/default/
+sudo wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/kimthostrup/bootstrap/main/grub -P /etc/default/
 sudo update-grub
-sudo shutdown -r now
+
+
+echo "Changing networking files"
+sudo cp /etc/network/interfaces{,.back$(date +%s)}
+sudo wget --no-check-certificate --content-disposition 
+
 echo "Rebooting....."
+sudo shutdown -r now
+
