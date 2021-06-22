e036ac9b5297eafabf148d2b6844931448df99d5
Kim Thostrup <kim@thostrup.dk>
2021-06-22 22:39:17 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 echo "adding wan-admin to sudoers file"
-sudo cp /etc/sudoers{,.back$(date +%s)}
+cp /etc/sudoers{,.back$(date +%s)}
 echo "wan-admin ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
 
 echo "Disabling netplan and changing interface names"
-sudo apt install ifupdown net-tools
-sudo mv /etc/default/grub /etc/default/grub.org
-sudo wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/kimthostrup/bootstrap/main/grub -P /etc/default/
-sudo update-grub
+apt install ifupdown net-tools
+mv /etc/default/grub /etc/default/grub.org
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/kimthostrup/bootstrap/main/grub -P /etc/default/
+update-grub
 
-sudo rm -rf /etc/update-motd.d/95-hwe-eol
-sudo rm -rf /etc/update-motd.d/88-esm-announce
-sudo rm -rf /etc/update-motd.d/91-contract-ua-esm-status
-sudo rm -rf /etc/update-motd.d/91-release-upgrade
-sudo rm -rf /etc/update-motd.d/50-motd-news
+rm -rf /etc/update-motd.d/95-hwe-eol
+rm -rf /etc/update-motd.d/88-esm-announce
+rm -rf /etc/update-motd.d/91-contract-ua-esm-status
+rm -rf /etc/update-motd.d/91-release-upgrade
+rm -rf /etc/update-motd.d/50-motd-news
 
 echo "Changing networking files"
-sudo rm /etc/network/interfaces
-sudo wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/kimthostrup/bootstrap/main/interfaces -P /etc/network/
+rm /etc/network/interfaces
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/kimthostrup/bootstrap/main/interfaces -P /etc/network/
 
-ip_address=`ip -4 addr | grep -E 'eth|ens' | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+ip_address=`ip -4 addr | grep -E 'eth0' | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+
 printf "\n" >> /etc/issue
 echo "${c4}${c5}  _ _ _            _____         _            " > /etc/issue
 echo "${c4}${c5} | | | |___ ___   |_   _|___ ___| |_ ___ ___  " >> /etc/issue
