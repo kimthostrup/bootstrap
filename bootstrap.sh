1b87a84854ec1e96cbb93699199b2df4caa71b76
Kim Thostrup <390455+kimthostrup@users.noreply.github.com>
2021-06-30 15:28:30 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,6 +42,10 @@ systemctl disable --now systemd-resolved
 
 # Install ttyd
 apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev -y
+git clone https://github.com/tsl0922/ttyd.git
+cd ttyd && mkdir build && cd build
+cmake ..
+make && sudo make install
 
 #Changing the issues file
 rm -rf /etc/issue
